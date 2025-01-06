4479590cb0419cf2085001cb41828cda9da03dd7
devnessaa <vanessacodev@gmail.com>
2025-01-06 13:14:11 -0300

    06012025/003

---
+++ CURSOS LIVRES/UDEMY/Curso de Python 3 do basico ao avançado/AULAS/029_main.py
@@ -0,0 +1,12 @@
+from sys import path
+from package import modulo
+from package.modulo import *
+
+print(__name__)
+print()
+print(*path, sep='\n')
+print()
+print(soma(2,5))
+print()
+print(variavel)
+print(nova_variavel)
---
+++ CURSOS LIVRES/UDEMY/Curso de Python 3 do basico ao avançado/AULAS/package/modulo.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'soma',
+    'variavel',
+    'nova_variavel'
+]
+
+def soma(x,y):
+    return x + y
+
+variavel = 'Boa'
+nova_variavel = 'Tarde'
