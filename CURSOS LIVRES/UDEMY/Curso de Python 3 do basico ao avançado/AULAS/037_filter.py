c19725e0c78a36591ef8832fee58b896660e9952
devnessaa <vanessacodev@gmail.com>
2025-01-07 14:41:31 -0300

    07012025/004

---
+++ CURSOS LIVRES/UDEMY/Curso de Python 3 do basico ao avançado/AULAS/037_filter.py
@@ -0,0 +1,31 @@
+# filter é um filtro funcional
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+def filtrar_preco(produto):
+    return produto['preco'] > 100
+# novos_produtos = [
+#     p for p in produtos
+#     if p['preco'] > 100
+# ]
+novos_produtos = filter(
+    # lambda produto: produto['preco'] > 100,
+    filtrar_preco,
+    produtos
+)
+print_iter(produtos)
+print_iter(novos_produtos)
+
+
+"""
+- filter é uma ferramenta poderosa para filtrar elementos de um iterável com base em uma condição.
+- Ideal para cenários onde você precisa selecionar dados específicos de forma eficiente e legível.
+- Combina bem com outras funções funcionais como map e lambda, tornando o código mais expressivo e funcional.
+"""
