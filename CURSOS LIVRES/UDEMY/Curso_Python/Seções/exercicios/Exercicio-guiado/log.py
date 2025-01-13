350382fa96050b900805a65ad3b12afc57f2ee83
devnessaa <vanessacodev@gmail.com>
2025-01-13 16:59:33 -0300

    13012025/002

---
+++ CURSOS LIVRES/UDEMY/Curso_Python/Seções/exercicios/Exercicio-guiado/log.py
@@ -0,0 +1,37 @@
+# Abstração
+from pathlib import Path
+
+log_file = Path(__file__).parent / 'log.txt'
+
+class Log:
+    def _log(self,msg):
+        raise NotImplementedError('Implente o método log')
+    
+    def log_error(self,msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_sucess(self,msg):
+        return self._log(f'Sucess: {msg}')
+        
+    
+class LogFileMixing(Log):
+    
+    def _log(self,msg):
+        msgf = f'{msg}, em ({self.__class__.__name__})'
+        print(f'Salvando no log: {msg}')
+        with open(log_file, 'a') as arquivo:
+            arquivo.write(msgf)
+            arquivo.write('\n')
+
+class LogPrintMixing(Log):
+    def _log(self,msg):
+        print(f'{msg}, em ({self.__class__.__name__})')
+ 
+if __name__ == '__main__':
+    lp = LogPrintMixing()
+    lp.log_error('NOT OK')
+    lp.log_sucess('OK')
+    
+    lf = LogFileMixing()
+    lf.log_error('NOT OK')
+    lf.log_sucess('OK')
---
+++ CURSOS LIVRES/UDEMY/Curso_Python/Seções/exercicios/Exercicio-guiado/main.py
@@ -0,0 +1,7 @@
+from eletronico import Smartphone
+
+galazyA = Smartphone('Galaxy A')
+iphone11 = Smartphone('IPhone')
+
+galazyA.ligar()
+iphone11.desligar()
